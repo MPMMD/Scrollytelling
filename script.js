5a527a1fe3a57ee3a13726044abbaa08c0c259d9
Unknown <mart803d@edu.zealand.dk>
2022-09-30 14:45:03 +0200

    Switch
    Indholdet godkendt af Jesper.
    Video virker mere smooth why? idk.
    Ellers var der body movin plugin til after effects han snakkede om.

--- script.js
+++ script.js
@@ -46,3 +46,26 @@ function addObserver(el){
 }
 // Example usage
 scrollTrigger('.animateText')
+
+
+
+// // From Jespers Template
+// var frameNumber = 0, // start video at frame 0
+// // lower numbers = faster playback
+
+// playbackConst = 50, 
+// // get page height from video duration
+
+// vid = document.getElementById('bound-one'); 
+// // var vid = $('#v0')[0]; // jquery option
+
+
+
+// // Use requestAnimationFrame for smooth playback
+// function scrollPlay(){  
+// var frameNumber  = window.pageYOffset/playbackConst;
+// vid.currentTime  = frameNumber;
+// window.requestAnimationFrame(scrollPlay);
+// }
+
+// window.requestAnimationFrame(scrollPlay);
